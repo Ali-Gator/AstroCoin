ae1449b4d06562f2379b785345bf4e8b6494d283
Gleb Shkut <gleb.shkut@gmail.com>
2024-05-10 08:40:25 -0700

    update header

--- widgets/CommunityWidget/CommunityWidget.tsx
+++ widgets/CommunityWidget/CommunityWidget.tsx
@@ -4,19 +4,20 @@ import { ICommunityWidget } from '@/widgets/CommunityWidget/types';
 import { EPlanetType, PlanetItem } from '@/components/PlanetItem';
 import styles from './CommunityWidget.module.css';
 import token from '@/public/main-token.svg';
-
+import { useBoundStore } from '@/store';
 
 export const CommunityWidget: FC<ICommunityWidget> = ({
   imageSrc,
   imageAlt,
 }) => {
+  const { balance } = useBoundStore((state) => state);
   return (
     <div className={styles.communityWidget}>
       <Image src={imageSrc} alt={imageAlt ?? ''} />
       <div className={styles.infoWrapper}>
         <p className={styles.title}>XSYCOIN COMMUNITY</p>
         <div className={styles.tokenWrapper}>
-          <p className={styles.sum}>14,593,386</p>
+          <p className={styles.sum}>{balance.toFixed(2)}</p>
           <Image className={styles.coin} src={token} alt={'main token'} />
         </div>
       </div>
