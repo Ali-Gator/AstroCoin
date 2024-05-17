d0a2f16d4f357b90a354da2bbc8bee808ff49ea6
Gleb Shkut <gleb.shkut@gmail.com>
2024-05-17 07:56:56 -0700

    add ton wallet address

--- app/earn/page.tsx
+++ app/earn/page.tsx
@@ -2,14 +2,17 @@
 
 import { TasksList } from './components/TasksList';
 import { TopHeader } from './components/TopHeader';
-import { TonConnectButton } from '@tonconnect/ui-react';
+import { TonConnectButton, useTonAddress } from '@tonconnect/ui-react';
 
 export default function EarnPage() {
+  const address = useTonAddress();
+
   return (
     <div className="w-full flex flex-col items-center gap-8 p-4">
       <TopHeader />
       <TasksList />
       <TonConnectButton />
+      {address && <span>{address}</span>}
     </div>
   );
 }
