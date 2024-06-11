e5c6acd4c26f7d347c8a193499f725733866fcf4
Oleg G <ogordienov@yandex.ru>
2024-06-11 20:03:18 +0200

    update popup invite frens text

---
+++ app/frenx/components/InvitePopup.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@/shared/ui/Button';
+
+export const InvitePopup = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-between">
+      <div className="flex flex-col items-center">
+        <p className="text-[32px] font-termina600">Invite your frens</p>
+        <p className="text-2xl text-center">and enjoy incredible rewards:</p>
+        <p className="text-lg mt-10 self-start">
+          [ 10% ] of the mining earnings from each fren you refer
+        </p>
+        <p className="text-lg mt-4 self-start">
+          [ 5% ] of the mining earnings from the frens they refer
+        </p>
+        <p className="text-lg mt-4 self-start">
+          [ 2.5% ] of the mining earnings from the frens of those frens
+        </p>
+      </div>
+      <Button>Invite a friend</Button>
+    </div>
+  );
+};
