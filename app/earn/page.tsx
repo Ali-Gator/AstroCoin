52676ec1b170c42e35424bfc673a8fb5c1a6fe0a
Oleg G <ogordienov@yandex.ru>
2024-06-27 12:43:37 +0400

    delete useTonAddress

--- app/earn/page.tsx
+++ app/earn/page.tsx
@@ -2,18 +2,9 @@
 
 import { TasksList } from './components/TasksList';
 import { TopHeader } from './components/TopHeader';
-import {
-  TonConnectButton,
-  useTonAddress,
-  useTonWallet,
-} from '@tonconnect/ui-react';
+import { TonConnectButton } from '@tonconnect/ui-react';
 
 export default function EarnPage() {
-  const address = useTonAddress();
-  const wallet = useTonWallet();
-
-  console.log('wallet: ', wallet);
-
   return (
     <div className="w-full flex flex-col items-center gap-8 p-4">
       <TopHeader />
@@ -21,7 +12,6 @@ export default function EarnPage() {
       <div className="fixed bottom-8">
         <TonConnectButton />
       </div>
-      {address && <span>{address}</span>}
     </div>
   );
 }
