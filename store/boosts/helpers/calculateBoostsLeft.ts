caea0905b8bd0b960c26257ab1c7de76827155ef
glebshkut <gleb.shkut@gmail.com>
2024-07-02 18:53:56 -0700

    fix seconds bug

--- store/boosts/helpers/calculateBoostsLeft.ts
+++ store/boosts/helpers/calculateBoostsLeft.ts
@@ -1,6 +1,6 @@
 import { getSecondsPassed } from '@/helpers/convertTimeFormat';
 import { BoostDbData, BoostInterface } from '../type';
-import { MS_IN_HOUR } from '@/helpers/constants/time';
+import { S_IN_HOUR } from '@/helpers/constants/time';
 
 export const calculateBoostsLeft = (
   boost: BoostDbData & {
@@ -8,14 +8,14 @@ export const calculateBoostsLeft = (
     resetDuration: BoostInterface['resetDuration'];
   },
 ) => {
-  const { updatedAt, itemsLeft, maxItems, resetDuration } = boost;
+  const { updatedAt, itemsLeft, maxItems, resetDuration, type } = boost;
 
   const secondsPassed = getSecondsPassed(updatedAt);
   if (resetDuration === 'immediately') {
     return maxItems;
   }
 
-  const resetDurationInSeconds = resetDuration * MS_IN_HOUR;
+  const resetDurationInSeconds = resetDuration * S_IN_HOUR;
   const itemsToAdd = Math.floor(secondsPassed / resetDurationInSeconds);
   const newItemsLeft = itemsLeft + itemsToAdd;
   return newItemsLeft > maxItems ? maxItems : newItemsLeft;
